0893f696ff63f6a969185b4fba5c1d2322a4ded6
Darwin Luque <darwin.luque.98@gmail.com>
2022-10-08 15:02:38 -0600

    chore: create a query bus

---
+++ buses/query-bus.ts
@@ -0,0 +1,25 @@
+import { Type } from '../utils/types'
+
+export interface IQuery {}
+
+export interface IQueryHandler<TQuery extends IQuery = any, TResult = any> {
+  query: Type<TQuery>
+  execute(query: TQuery): Promise<TResult>
+}
+
+export class QueryBus {
+  private handlers: IQueryHandler[] = []
+
+  registerHandlers(...handlers: IQueryHandler[]) {
+    this.handlers = handlers
+  }
+
+  execute(query: IQuery) {
+    const handler = this.handlers.find((h) => query instanceof h.query)
+    if (!handler) {
+      throw new Error(`No handler found for query ${query.constructor.name}`)
+    }
+
+    return handler.execute(query)
+  }
+}
